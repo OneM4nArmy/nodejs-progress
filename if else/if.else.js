0135e1926fc73dffeef026bb96060b73b1d12d52
OneM4nArmy <127156154+OneM4nArmy@users.noreply.github.com>
2023-03-09 18:55:28 +0100

    Add files via upload

---
+++ if else/if.else.js
@@ -0,0 +1,14 @@
+const name = "unknown try n find me.";
+let age = 1111;
+
+console.log(name);
+
+if (age === 1111) {
+    console.log("do some searching :D")
+} else {
+    console.log("imagine ...")
+}
+
+if (age > 1110) {
+    console.log(`older than ${age}`)
+}
